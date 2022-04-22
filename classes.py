844592d7a22a1f7253bacc317941c4017aeb91db
MyuKsaii <guardian22802@gmail.com>
2022-04-22 16:04:49 -0500

    Update classes.py
    Added description for '__init__()' function
    
    Added description for 'power()' function
    
    Added description for 'channel_up()' funtion
    
    Added description for 'channel_down()' function
    
    Added description for 'volume_up()'
    function
    
    Added description for 'volume_down()'function
    
    Added description for '__str__()' function

--- classes.py
+++ classes.py
@@ -6,56 +6,44 @@ class Television:
     MAX_VOLUME = 2      # Maximum TV volume
 
     def __init__(self):
-       
+       '''Function for setting default variable values
+        during module initialization
+        '''
         self.__channel = Television.MIN_CHANNEL
         self.__volume = Television.MIN_VOLUME
         self.__status = False
 
     def power(self):
-        
+        '''Function for turning the television on and off'''
         self.__status = not(self.__status)            
 
 
     def channel_up(self):
-
+        '''Function for increasing the channel of television'''
         if self.__status:
             self.__channel = (self.__channel + 1)%(Television.MAX_CHANNEL +1)
 
-        '''
-        if self.__status:
-            if self.__channel < Television.MAX_CHANNEL:
-                self.__channel += 1
-            elif self.__channel == Television.MAX_CHANNEL:
-                self.__channel = Television.MIN_CHANNEL
-       '''
-     
 
     def channel_down(self):
-       
+       '''Function for decreasing the channel of televsion'''
         if self.__status:
             self.__channel = (self.__channel - 1)%(Television.MAX_CHANNEL +1)
-        '''
-        if self.__status:
-            if self.__channel > Television.MIN_CHANNEL:
-                self.__channel -= 1
-            elif self.__channel == Television.MIN_CHANNEL:
-                self.__channel = Television.MAX_CHANNEL
-        '''
      
 
     def volume_up(self):
-       
+       '''Function for increasing the volume of television'''
         if self.__status and self.__volume < Television.MAX_VOLUME:
             self.__volume += 1
       
 
     def volume_down(self):
-        
+        '''Function for decreasing the volume of television'''
         if self.__status and self.__volume > Television.MIN_VOLUME:
             self.__volume -= 1
   
 
     def __str__(self):
-       
+       '''Function to return formatted string displaying television status
+       whenever printed'''
         return f'TV status: Is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
         
