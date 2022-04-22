49718c74a12225a9e2b0b00b68be89af8bb8ba14
MyuKsaii <guardian22802@gmail.com>
2022-04-22 16:33:36 -0500

    Update classes.py
    Added type hinting under __init__()

--- classes.py
+++ classes.py
@@ -5,44 +5,44 @@ class Television:
     MIN_VOLUME = 0      # Minimum TV volume
     MAX_VOLUME = 2      # Maximum TV volume
 
-    def __init__(self):
+    def __init__(self) -> None:
        '''Function for setting default variable values
         during module initialization
         '''
-        self.__channel = Television.MIN_CHANNEL
-        self.__volume = Television.MIN_VOLUME
-        self.__status = False
+        self.__channel: int = Television.MIN_CHANNEL
+        self.__volume: int = Television.MIN_VOLUME
+        self.__status: bool = False
 
-    def power(self):
+    def power(self) -> None:
         '''Function for turning the television on and off'''
         self.__status = not(self.__status)            
 
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         '''Function for increasing the channel of television'''
         if self.__status:
             self.__channel = (self.__channel + 1)%(Television.MAX_CHANNEL +1)
 
 
-    def channel_down(self):
+    def channel_down(self) -> None:
        '''Function for decreasing the channel of televsion'''
         if self.__status:
             self.__channel = (self.__channel - 1)%(Television.MAX_CHANNEL +1)
      
 
-    def volume_up(self):
+    def volume_up(self) -> None:
        '''Function for increasing the volume of television'''
         if self.__status and self.__volume < Television.MAX_VOLUME:
             self.__volume += 1
       
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         '''Function for decreasing the volume of television'''
         if self.__status and self.__volume > Television.MIN_VOLUME:
             self.__volume -= 1
   
 
-    def __str__(self):
+    def __str__(self) -> str:
        '''Function to return formatted string displaying television status
        whenever printed'''
         return f'TV status: Is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
